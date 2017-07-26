6ff3af64d9d294d52243aca0a91f508b126f5f68
Chris Dyer <chris.dyer11@gmail.com>
2017-07-25 18:05:35 -0600

    Finished hover stop and user controls.

--- js/slider.js
+++ js/slider.js
@@ -37,3 +37,45 @@ function startSlider() {
 	}, 3000);
 
 };
+
+function prev() {
+	newSlide = sliderInt - 1;
+	showSlide(newSlide);
+}
+function next() {
+	newSlide = sliderInt + 1;
+	showSlide(newSlide);
+}
+
+function stopLoop() {
+	window.clearInterval(loop);
+
+}
+
+function showSlide(id) {
+	stopLoop();
+	if(id > count) {
+			id = 1;
+			
+		} else if(id<1) {
+			id = count;
+		};
+
+		// Fade out one, fade in next.
+
+		$('#slider > img').fadeOut(300);
+		$('#slider > img#' + id).fadeIn(300);
+
+		sliderInt = id;
+		sliderNext = id + 1;
+		startSlider();
+}
+
+$("#slider > img").hover(
+	function() {
+		stopLoop();
+	},
+	function() {
+		startSlider();
+	}
+)
